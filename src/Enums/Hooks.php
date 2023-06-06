5f6fba46f9a796aa83f60d326bb842b4dcc36999
prismathic <pjjsalam1999@gmail.com>
2023-06-06 22:32:45 +0100

    added enum class for hooks

---
+++ src/Enums/Hooks.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Prismaticode\MakerChecker\Enums;
+
+abstract class Hooks
+{
+    public const POST_INITIATE = 'post_initiate';
+    public const PRE_APPROVAL = 'pre_approval';
+    public const POST_APPROVAL = 'post_approval';
+    public const PRE_REJECTION = 'pre_rejection';
+    public const POST_REJECTION = 'post_rejection';
+    public const ON_FAILURE = 'on_failure';
+
+    public static function getAll(): array
+    {
+        return [
+            static::POST_INITIATE,
+            static::PRE_APPROVAL,
+            static::POST_APPROVAL,
+            static::PRE_REJECTION,
+            static::POST_REJECTION,
+            static::ON_FAILURE,
+        ];
+    }
+}
