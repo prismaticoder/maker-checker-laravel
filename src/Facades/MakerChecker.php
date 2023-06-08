73ac5f8fd62124a2de721f489db4099293ea900c
prismathic <pjjsalam1999@gmail.com>
2023-06-08 21:16:45 +0100

    add docblocks to facade class

--- src/Facades/MakerChecker.php
+++ src/Facades/MakerChecker.php
@@ -5,6 +5,17 @@ namespace Prismaticode\MakerChecker\Facades;
 use Illuminate\Support\Facades\Facade;
 use Prismaticode\MakerChecker\MakerCheckerRequestManager;
 
+/**
+ * @method static \Prismaticode\MakerChecker\RequestBuilder request()
+ * @method static void afterInitiating(\Closure $callback)
+ * @method static void afterApproving(\Closure $callback)
+ * @method static void afterRejecting(\Closure $callback)
+ * @method static void onFailure(\Closure $callback)
+ * @method static \Prismaticode\MakerChecker\Contracts\MakerCheckerRequestInterface approve(\Prismaticode\MakerChecker\Contracts\MakerCheckerRequestInterface $request, \Illuminate\Database\Eloquent\Model $approver, string|null $remarks)
+ * @method static \Prismaticode\MakerChecker\Contracts\MakerCheckerRequestInterface reject(\Prismaticode\MakerChecker\Contracts\MakerCheckerRequestInterface $request, \Illuminate\Database\Eloquent\Model $rejector, string|null $remarks)
+ *
+ * @see \Prismaticode\MakerChecker\MakerCheckerRequestManager
+ */
 class MakerChecker extends Facade
 {
     protected static function getFacadeAccessor()
